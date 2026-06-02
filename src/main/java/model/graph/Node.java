f6ef8547bd51ceaeead0b141c3489c985f85fa94
Sanem <sanemhimana@hotmail.com>
2026-06-02 22:36:10 +0200

    Update Node.java

--- src/main/java/model/graph/Node.java
+++ src/main/java/model/graph/Node.java
@@ -1,5 +1,21 @@
 package model.graph;
 
 public class Node {
-    
+    private int id;
+    private String name;
+    private double x;
+    private double y;
+    private int maxCapacity;
+    private boolean closed;
+
+    public Node(int id, String name, double x, double y) {
+        this.id = id;
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.closed = false;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
 }
