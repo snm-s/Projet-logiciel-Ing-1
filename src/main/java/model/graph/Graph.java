4033d80f3826ff712c37a1ce500e8b7d8c8c20fb
Sanem <sanemhimana@hotmail.com>
2026-06-02 22:35:54 +0200

    Update Graph.java

--- src/main/java/model/graph/Graph.java
+++ src/main/java/model/graph/Graph.java
@@ -1,5 +1,14 @@
 package model.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
-    
+    private List<Node> nodes = new ArrayList<>();
+    private List<Edge> edges = new ArrayList<>();
+
+    public void addNode(Node n) { nodes.add(n); }
+    public void addEdge(Edge e) { edges.add(e); }
+    public List<Node> getNodes() { return nodes; }
+    public List<Edge> getEdges() { return edges; }
 }
