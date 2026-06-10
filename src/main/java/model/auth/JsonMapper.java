89591d4603e31e81e42433f0e70aa11f0ba7fc74
mrtl-mtlp <mouttalapanemartial1@gmail.com>
2026-06-10 04:48:15 +0200

    feat: Ajouter des fichiers JSON pour les utilisateurs et les zones, et implémenter le service de données de simulation

---
+++ src/main/java/model/auth/JsonMapper.java
@@ -0,0 +1,16 @@
+package model.auth;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonMapper {
+
+    private JsonMapper() {}
+
+    public static final ObjectMapper INSTANCE = new ObjectMapper()
+        .registerModule(new JavaTimeModule())
+        .enable(SerializationFeature.INDENT_OUTPUT)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+}
